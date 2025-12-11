30e964e672586a1f56a7e707c29c1324ab249e31
Moritz <moritz.eigenauer@gmail.com>
2025-12-11 18:30:52 +0100

    fix: use wget instead of curl

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ echo "[*] creating temporary directory"
 TEMP_DIR=$(mktemp -d)
 
 echo "[*] downloading latest release"
-curl -L -o "$TEMP_DIR/hid-proxy_aarch64.tar.gz" "https://github.com/EiSiMo/hid-proxy/releases/latest/download/hid-proxy_aarch64.tar.gz"
+wget -O "$TEMP_DIR/hid-proxy_aarch64.tar.gz" "https://github.com/EiSiMo/hid-proxy/releases/latest/download/hid-proxy_aarch64.tar.gz"
 
 echo "[*] extracting archive"
 tar -xzf "$TEMP_DIR/hid-proxy_aarch64.tar.gz" -C "$TEMP_DIR"
